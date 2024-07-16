adcd686f4252af1864dc5f27389eec5ddeafca92
FLINT921 <roman.alex9009@gamil.com>
2024-07-16 15:20:49 +0300

    Commit taskHooks 0.2.5

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState, useCallback, useMemo } from 'react';
 
-import Header from '../Header';
-import Main from '../Main';
-import Footer from '../Footer';
+import { Header } from '../Header/Header';
+import { Main } from '../Main/Main';
+import { Footer } from '../Footer/Footer';
 
 export const App = () => {
   const initialTasks = [
